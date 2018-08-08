f96fb7f361e4cefeccff49f587fb44818a32a1b3
jamiahuswalton <jamiahuswalton@gmail.com>
2018-08-08 11:43:43 -0500

    Changes the initial screen launched to "StartSCreen" activity

---
+++ app/src/main/java/com/jamiahus/getpartcode/StartScreenActivity.java
@@ -0,0 +1,14 @@
+package com.jamiahus.getpartcode;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class StartScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+
+}
